95ccf3fe40fdafef29951a565386c99a155f37c5
Shane Kelly <shaneqkelly@gmail.com>
2019-04-14 23:13:55 -0400

    Move MongoDB initialization to top.

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,14 @@ import os
 from flask import Flask, render_template
 from pymongo import MongoClient
 
+# Create a MongoDB client that connects to the MongoDB daemon running on a MongoDB server. The
+#   MongoDB URI has the IP address of the server where we are running our MongoDB daemon, so that
+#   it knows where to connect. The Herokuapp already has the MongoDB URI defined in its
+#   environment variables, but if you want to run this code locally, then you will need to ask
+#   for the secret MongoDB URI we use.
+mongo_client = MongoClient(os.environ['MONGODB_URI'])
+db = mongo_client.test_database # use a test database for now
+collection = db.test_collection # use a test collection for now
 
 app = Flask(__name__)
 
@@ -18,14 +26,5 @@ def home():
 
 
 if __name__ == '__main__':
-  # Create a MongoDB client that connects to the MongoDB daemon running on a MongoDB server. The
-  #   MongoDB URI has the IP address of the server where we are running our MongoDB daemon, so that
-  #   it knows where to connect. The Herokuapp already has the MongoDB URI defined in its
-  #   environment variables, but if you want to run this code locally, then you will need to ask
-  #   for the secret MongoDB URI we use.
-  mongo_client = MongoClient(os.environ['MONGODB_URI'])
-  db = mongo_client.test_database # use a test database for now
-  collection = db.test_collection # use a test collection for now
-
   app.config['DEBUG'] = True
   app.run() # run the Flask app
