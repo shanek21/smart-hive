6013bc70b6d64ad321550b761cbf9b63ef07c526
Anisha Nakagawa <anisha.nakagawa@gmail.com>
2019-04-20 16:01:25 -0400

    adding graph labels

--- app/static/index.js
+++ app/static/index.js
@@ -6,11 +6,6 @@ window.onload = function() {
 
 window.addEventListener("resize", createWeightSVG);
 
-// $( window ).resize(function() {
-//   createWeightSVG();
-//   $('#weightSVG').remove();
-// });
-
 function loadData() {
   // console.log("load data");
 
@@ -59,9 +54,6 @@ function createWeightSVG(){
       .attr("width", svg_width)
       .attr("height", svg_height)
       .attr("id", "weightSVG")
-    // $weightSVG.attr("width", svg_width).attr("height", svg_height);
-
-
 
     // setup bounds
     console.log(data);
@@ -74,7 +66,6 @@ function createWeightSVG(){
     data["data"].forEach(function(entry){
       let date = new Date(entry.timestamp)
       if (entry.topic == "weight" && date.getHours()==0){
-        // console.log(entry);
 
         let weight = entry.value
         let hiveName = entry.hive_name
@@ -85,7 +76,6 @@ function createWeightSVG(){
           GLOBAL.hives.push(hiveName)
         }
 
-        // console.log(date);
         if (date.getTime() < minDate.getTime()){ minDate = date;}
         if (date.getTime() > maxDate.getTime()){ maxDate = date;}
         if (weight < minWeight){ minWeight = weight;}
@@ -93,18 +83,13 @@ function createWeightSVG(){
       }
     });
 
-    console.log(minDate);
-    console.log(maxDate);
-    console.log(minWeight);
-    console.log(maxWeight);
-    console.log(GLOBAL);
-
-    // make axis
-    // let $x_axis = $('line').x1(10).y1(10).x2(20).y2(20)
-    let x_min = 10;
-    let x_max = svg_width-10;
-    let y_min = svg_height-10;
-    let y_max = 10;
+    // make axis and labels
+    let x_margin = 50;
+    let y_margin = 25;
+    let x_min = x_margin;
+    let x_max = svg_width-x_margin;
+    let y_min = svg_height-y_margin;
+    let y_max = y_margin;
     weightSVG.append("line")
       .attr("x1", x_min)
       .attr("y1", y_min)
@@ -117,52 +102,85 @@ function createWeightSVG(){
       .attr("x2", x_min)
       .attr("y2", y_max)
       .style("stroke", "999999");
+    weightSVG.append("text")
+      .text(formatDate(minDate))
+      .attr("x", x_min)
+      .attr("y", y_min + 15)
+      .style("fill", "#999999");
+    weightSVG.append("text")
+      .text(formatDate(maxDate))
+      .attr("x", x_max)
+      .attr("y", y_min + 15)
+      .style("fill", "#999999")
+      .attr("text-anchor", "end")
+    weightSVG.append("text")
+      .text(Math.round(minWeight) + " lb")
+      .attr("x", x_min - 5)
+      .attr("y", y_min)
+      .style("fill", "#999999")
+      .attr("text-anchor", "end")
+    weightSVG.append("text")
+      .text(Math.round(maxWeight) + " lb")
+      .attr("x", x_min - 5)
+      .attr("y", y_max)
+      .style("fill", "#999999")
+      .attr("text-anchor", "end")
 
-    // data["data"].forEach(function(entry){
-    //   let date = new Date(entry.timestamp)
-    //   if (entry.topic == "weight" && date.getHours()==0){
+      // create svg elements for data
+    let nameLabels = [];
     GLOBAL.weight = {}
     GLOBAL.hives.forEach(function(hiveName){
       GLOBAL.weight[hiveName] = []
+      let lastHiveWeight = 0;
+      let color = '#888'
+      if (hiveName == "hive_1"){color = "#3a81a0"};
+      if (hiveName == "hive_2"){color = "#618c4e"};
+
       weightEntries[hiveName].forEach(function(entry){
         let date = new Date(entry.timestamp)
         let weight = entry.value
-        let color = '#888'
-        let hiveName = entry.hive_name
-        if (hiveName == "hive_1"){color = "#3a81a0"};
-        // if (entry.topic.includes("hive_2")){color = "8073c2"};
-        if (hiveName == "hive_2"){color = "#618c4e"};
-        // console.log(date);
         let dataPoint = weightSVG.append("circle")
           .attr("cx", map_range(date.getTime(), minDate.getTime(), maxDate.getTime(), x_min, x_max))
           .attr("cy", map_range(weight, minWeight, maxWeight, y_min, y_max))
           .attr("r", 2)
           .style("fill", color);
         GLOBAL.weight[hiveName].push(dataPoint)
+
+        lastHiveWeight = weight;
       })
+      console.log(lastHiveWeight);
+      let nameLabel = weightSVG.append("text")
+        .text(hiveName)
+        .attr("x", x_max + 5)
+        .attr("y", map_range(lastHiveWeight, minWeight, maxWeight, y_min, y_max))
+        .style("fill", color)
+        .style("font-weight", "bold")
+      nameLabels.push(nameLabel);
     })
-    //     // console.log(entry);
-    //     // let date = new Date(entry.timestamp)
-    //     let weight = new Date(entry.value)
-    //     let color = '#888'
-    //     let hiveName = entry.hive_name
-    //     if (hiveName == "hive_1"){color = "#3a81a0"};
-    //     // if (entry.topic.includes("hive_2")){color = "8073c2"};
-    //     if (hiveName == "hive_2"){color = "#618c4e"};
-    //     // console.log(date);
-    //     let dataPoint = weightSVG.append("circle")
-    //       .attr("cx", map_range(date.getTime(), minDate.getTime(), maxDate.getTime(), x_min, x_max))
-    //       .attr("cy", map_range(weight, minWeight, maxWeight, y_min, y_max))
-    //       .attr("r", 2)
-    //       .style("fill", color);
-    //
-    //   }
-    //
-    // });
+    console.log(nameLabels);
+
+    // sort by vertical order of names
+    nameLabels.sort(function(a,b){
+      return a.attr("y") - b.attr("y")
+    })
+    // space out hive names if too close
+    for (let n = 1; n < nameLabels.length; n++){
+      let prev_y = Number(nameLabels[n-1].attr("y"))
+      let current_y = Number(nameLabels[n].attr("y"))
+      console.log(prev_y, current_y, prev_y+50);
+      if (current_y < prev_y+15){
+        nameLabels[n].attr("y", prev_y+15);
+      }
+    }
+
   }
 
 }
 
 function map_range(value, low1, high1, low2, high2) {
   return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+function formatDate(date) {
+  return date.toLocaleString('en-us', { month: 'long' }) + " " + date.getFullYear();
 }
